37737082b75ae4f431c6454ea457006cf7b96b52
Danhao Ding <danhaoding@gmail.com>
2019-06-14 01:09:10 +0800

    bash script for grid search

--- HINGCN-GS/pytorch-graphsage-master/run.sh
+++ HINGCN-GS/pytorch-graphsage-master/run.sh
@@ -16,7 +16,7 @@ for p in $prep; do
 for a in $aggr; do
 for e in $edge; do
 python3 ./train.py \
-    --problem-path ../../data/freebase \
+    --problem-path ../../data/freebase/ \
     --problem yago \
     --epochs 1000 \
     --batch-size 2048 \
@@ -24,9 +24,9 @@ python3 ./train.py \
     --lr-schedule constant\
     --dropout 0.5\
     --batchnorm\
-    --prep-class $prep \
-    --edgeupt-class $edge \
-    --aggregator-class $aggr \
+    --prep-class $p \
+    --edgeupt-class $e \
+    --aggregator-class $a \
     --log-interval 1\
     > "experiment/freebase/fb_"$count".txt" &
 let count++
